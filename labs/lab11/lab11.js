174da55f744bb9561742c7d11f02c558ec653abb
Unknown <sam.lindsey@du.edu>
2019-10-23 16:12:58 -0600

    Completed Lab 10

---
+++ labs/lab11/lab11.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> Practice ES6! </title>
+
+  <script>
+//import {test} from './class';
+// export class blah
+
+// export (addsum, multsum)
+// import
+// ?*class.js */
+// export class test {
+//    getSum(a,b){
+//    return sum(a,b);
+//    }
+// }
+class Rectangle {  constructor(height, width) {    this.height = height;    this.width = width;  }  get area() {    return this.calcArea();  }  calcArea() {    return this.height * this.width;  }}
+</script>
+</head>
+</html>
+
+//write it in es6 console
+//don't do EXPORT until you turn it inspect
+
+//export Lab11 {
+//foo(){
+//return "bar";
+//}
+//}
+//let myLab = new Lab11
+//console.log(myLab.foo)
+
+//argument =100
+
+//console.log('${foo}${bar}``')
+
+//console.log(`I am a multilin string.
+//so great`)
