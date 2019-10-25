a11bd67ee2b032f3f6b9b1e8c5aee9b403d72d8b
Unknown <sam.lindsey@du.edu>
2019-10-25 16:34:42 -0600

    Completed Lab 11
    -I had some trouble getting the file to run on the es6 console online.... I was also unsure exactly of the header and correct tags to ensure the .js file would be properly imported when you download the file and try to run it. The code is all solid though, just those little bits around it that may be messed up.

--- labs/lab11/lab11.js
+++ labs/lab11/lab11.js
@@ -3,8 +3,55 @@
 <head>
   <meta charset="utf-8">
   <title> Practice ES6! </title>
-
   <script>
+  //I am unsure if any of these acutally work as I was unable to get the es6 console to even function
+  //after creating a new "lab11" object and trying to log to console... no luck... I think they are correct though!
+  export class lab11{
+   testDefaultParameters(first, second=100){
+     retObj = { "one":first, "two":second };
+     //console.log(retObj);
+     return retObj;
+   }
+
+    testTemplateLiterals(firstName, middleName, lastName){
+      var nameTemplate = `${firstName} ${','} ${middleName} ${','} ${lastName}`;
+      return nameTemplate;
+    }
+
+    testMultilineStrings(){
+      var longBoi = `   This
+      goes
+ 	 	across
+ 					multiple
+ lines!!
+  									!!!!!!
+  :)
+ 				!!!!!!`
+    }
+
+    testSortWithArrowFunction(numArray){
+      numArray.sort(sortArray => sortArray);
+    }
+
+    testSortWihtoutArrowFunction(numArr){
+      numArr.sort(function(a,b) {return (b-a)});
+    }
+  }
+ // let myLab = new Lab11;
+ //
+ // console.log(myLab.testDefaultParameters (10));
+ //
+ // console.log(myLab.testTemplateLiterals ('Sam','Joseph','Lindsey'));
+ //
+ // console.log(myLab.testMultilineStrings());
+ //
+ // let numAr = [2,6,1,5];
+ // console.log(myLab.testSortWithArrowFunction(numAr));
+ // console.log(myLab.testSortWithoutArrowFunction(numAr));
+</script>
+</head>
+</html>
+
 //import {test} from './class';
 // export class blah
 
@@ -16,10 +63,6 @@
 //    return sum(a,b);
 //    }
 // }
-class Rectangle {  constructor(height, width) {    this.height = height;    this.width = width;  }  get area() {    return this.calcArea();  }  calcArea() {    return this.height * this.width;  }}
-</script>
-</head>
-</html>
 
 //write it in es6 console
 //don't do EXPORT until you turn it inspect
